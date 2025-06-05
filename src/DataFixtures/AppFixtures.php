27c4167d082d93b665b8289ff6e6d976482daa60
Shimouna-14 <chimene.ndeby@gmail.com>
2025-06-06 01:24:05 +0200

    Réorganiser les importations et ajouter l'image par défaut pour les médecins dans les fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,11 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
-use App\Entity\Doctor;
 use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Doctor;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
@@ -15,12 +16,12 @@ class AppFixtures extends Fixture
 
         $specialities = ["Chirurgie", "Dentiste", "Pédiatrie", "Urgence", "Gynécologie", "Généraliste"];
 
-
         for ($i = 0; $i < 30; $i++) {
             $doctor = new Doctor();
             $doctor->setFirstname($faker->firstName());
             $doctor->setLastname($faker->lastName());
             $doctor->setAddress($faker->streetAddress());
+            $doctor->setImage("https://avatar.iran.liara.run/public");
             $doctor->setSpeciality($faker->randomElement($specialities));
             $doctor->setCity($faker->city());
             $doctor->setZip($faker->postcode());
@@ -28,6 +29,14 @@ class AppFixtures extends Fixture
 
             $manager->persist($doctor);
         }
+
+        $admin = new User();
+        $admin->setEmail('admin@admin.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword('$2y$13$YKJVWg56HofbhjpzsPPv9uY/FiEQymZRtTMUIf/fCMYkby0cgtuZ6'); // password: admin123
+        $admin->setFirstname('Admin');
+        $admin->setLastname('Admin');
+        $manager->persist($admin);
 
         $manager->flush();
     }
